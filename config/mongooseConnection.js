7bba749a8df62d3f17e49e1d3e0d10e7fc309187
Reena Singh <aditya.kumar@calidig.com>
2023-03-16 07:59:00 +0530

    small fix

--- config/mongooseConnection.js
+++ config/mongooseConnection.js
@@ -1,8 +1,11 @@
 require('dotenv').config({path:rootPath+'/.env'});
 const mongoose = require("mongoose");
 
-	mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?directConnection=true&serverSelectionTimeoutMS=2000`);
-
+	mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?directConnection=true&serverSelectionTimeoutMS=2000&authSource=admin&appName=mongosh+1.6.2`,
+  	{
+	    useNewUrlParser: true,
+	    useUnifiedTopology: true
+  	});
 
 
   	const db = mongoose.connection;
